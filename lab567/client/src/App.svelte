edc4a9625aad3a33da49e5d496515a2b573395db
kraftwerk28 <kefirchik3@gmail.com>
2020-12-12 02:03:17 +0200

    Add basic client to #5

---
+++ lab567/client/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  import axios from 'axios';
+  let authorized = false;
+  let register = { email: '', password: '' };
+  let login = { email: 'kefirchik3@gmail.com', password: '' };
+
+  function onSubmitLogin() {
+    axios.post('/login', login)
+      .then(() => authorized = true)
+  }
+  function onSubmitRegister() {
+    axios.post('/register', register)
+      .then(() => alert('Registered successfully'))
+  }
+</script>
+
+<main>
+  {#if !authorized}
+    <form on:submit|preventDefault={onSubmitLogin}>
+      <fieldset>
+        <legend>Login</legend>
+        <input placeholder="email" type="email" bind:value={login.email} />
+        <input
+          placeholder="password"
+          type="password"
+          bind:value={login.password}
+        />
+        <button type="submit">Login</button>
+      </fieldset>
+    </form>
+  {/if}
+  <form on:submit|preventDefault={onSubmitRegister}>
+    <fieldset>
+      <legend>Register</legend>
+      <input placeholder="email" type="email" bind:value={register.email} />
+      <input
+        placeholder="password"
+        type="password"
+        bind:value={register.password}
+      />
+      <button type="submit">Register</button>
+    </fieldset>
+  </form>
+</main>
+
